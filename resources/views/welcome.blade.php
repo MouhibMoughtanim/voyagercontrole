e2e8fb19c84b0509e5a725208c27675d355bd22d
MouhibMoughtanim <mouhibmoughtanim47@gmail.com>
2022-04-06 22:41:59 +0000

    main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <a href="http://voyager-controle.herokuapp.com/admin">Go to voyager</a>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
