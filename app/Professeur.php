50404f7f9beed692775f9d4e990d5fc8d3244717
MouhibMoughtanim <mouhibmoughtanim47@gmail.com>
2022-04-07 01:48:59 +0000

    first commit

--- app/Professeur.php
+++ app/Professeur.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App;
-
+use \auth;
 use Illuminate\Database\Eloquent\Model;
 
 
 class Professeur extends Model
 {
-    
+    public function scopeProfs($query)
+    {
+        if (Auth::user()->role_id == 2)
+        {
+            return $query->where('id_user', '=', Auth::user()->id);
+        }
+        
+    }
 }
